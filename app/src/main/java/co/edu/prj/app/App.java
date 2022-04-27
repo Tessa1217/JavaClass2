79a16184c0c4f7bad2b9103c090217e993033197
Tessa <kwonyuj1996@gmail.com>
2022-04-27 14:06:34 +0900

    first commit - main app

--- app/src/main/java/co/edu/prj/app/App.java
+++ app/src/main/java/co/edu/prj/app/App.java
@@ -3,25 +3,26 @@ package co.edu.prj.app;
 import co.edu.prj.stream.InPutStreamEx;
 import co.edu.prj.stream.OutPutStreamEx;
 import co.edu.prj.stream.ReaderEx;
+import co.edu.prj.stream.WriterEx;
 
 public class App {
     public static void main( String[] args ) {
 
-//        OutPutStreamEx ose = new OutPutStreamEx();
-//        ose.run();
-//        ose.run2(); 
-//        
-//        InPutStreamEx ise = new InPutStreamEx();
-//        ise.read();
-//        ise.readAry();
-    	
-//    	WriterEx we = new WriterEx();
-//    	we.writeChar();
-//    	we.writeString();
+        OutPutStreamEx ose = new OutPutStreamEx();
+        ose.run();
+        ose.run2(); 
         
-//        ReaderEx re = new ReaderEx();
-//        //re.read();
-//        re.readBuffer(); 
+        InPutStreamEx ise = new InPutStreamEx();
+        ise.read();
+        ise.readAry();
+    	
+    	WriterEx we = new WriterEx();
+    	we.writeChar();
+    	we.writeString();
+        
+        ReaderEx re = new ReaderEx();
+        re.read();
+        re.readBuffer(); 
     	
     }
 }
